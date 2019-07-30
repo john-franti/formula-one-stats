099b74a1e8a97183b3901a65813b1168400d59bb
John Franti <john.franti@flatironschool.com>
2019-07-30 11:11:21 -0700

    Updates

---
+++ precache-manifest.b8cb224a1702a0ce6017340aa1f9f608.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98f72154c439db4d48a6b910f229a1a",
+    "url": "/formula-one-stats/index.html"
+  },
+  {
+    "revision": "ef5b904c05796afb2678",
+    "url": "/formula-one-stats/static/css/main.05e7f65a.chunk.css"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/formula-one-stats/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "ef5b904c05796afb2678",
+    "url": "/formula-one-stats/static/js/main.a7c61d97.chunk.js"
+  },
+  {
+    "revision": "58112e5e39ab2ca4fbbe",
+    "url": "/formula-one-stats/static/js/runtime~main.12de9516.js"
+  }
+]);
